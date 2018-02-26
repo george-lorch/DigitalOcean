141f147e09b6c2fbd0bae6d867e2e81fc3da4d2c
George O. Lorch III <george.lorch@percona.com>
2018-02-26 11:05:23 -0700

    -Added quickie scripts to run all TDB and MYR suites respectively.

---
+++ myr_mtr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cpus=`ncpus`
+echo "Running on ${cpus} cpus"
+
+chmod +x ./mysql-test-run.pl
+LIBHOTBACKUP=$(find $(readlink -f ..) -type f -name 'libHotBackup.so')
+LIBEATMYDATA=$(find /usr/lib -name 'libeatmydata.so' | sort | head -n 1)
+if [ -z "$LIBEATMYDATA" ]; then
+    echo "****** NO LIBEATMYDATA FOUND ******"
+    exit 1
+fi
+
+LD_PRELOAD="${LIBEATMYDATA} ${LIBHOTBACKUP}" ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=30 --parallel=${cpus} --big-test --suite=rocksdb,rocksdb_rpl,rocksdb.sys_vars $@
