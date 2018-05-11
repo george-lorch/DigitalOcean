9210cb62dbe0b2e72dd1cb5ffec866d7a1b7ecb8
George O. Lorch III <george.lorch@percona.com>
2018-05-11 11:50:12 -0700

    Fix to github protocol and URLs

--- git-create-project.sh
+++ git-create-project.sh
@@ -34,10 +34,10 @@ cd ./${PROJECT}
 ln -s `find ~/ -name boost_1_59_0 -type d | sort -n -s | tail -n 1`
 ln -s `find ~/ -name boost_1_65_0 -type d | sort -n -s | tail -n 1`
 
-git clone --recursive git@github.com:georgelorchpercona/myrocks &
+git clone --recursive https://github.com/georgelorchpercona/myrocks &
 clone1=$!
 
-git clone --recursive git@github.com:percona/percona-server &
+git clone --recursive https://github.com/percona/percona-server &
 clone2=$!
 mkdir percona-server-build-5.6
 mkdir percona-server-build-5.7
@@ -47,7 +47,7 @@ mkdir percona-server-install-5.7
 mkdir percona-server-install-8.0
 mkdir perconaft-build
 
-git clone --recursive git@github.com:facebook/mysql-5.6 facebook-mysql &
+git clone --recursive https://github.com/facebook/mysql-5.6 facebook-mysql &
 clone3=$!
 mkdir facebook-mysql-build
 mkdir facebook-mysql-install
@@ -58,11 +58,14 @@ wait_for_pid_to_disappear $clone3
 
 cd ../${PROJECT}/percona-server
 git remote add downstream git@github.com:georgelorchpercona/percona-server
+cd -
 
 cd ../${PROJECT}/percona-server/storage/tokudb/PerconaFT
 git remote add downstream git@github.com:georgelorchpercona/PerconaFT
+cd -
 
 cd ../${PROJECT}/facebook-mysql
 git remote add downstream git@github.com:georgelorchpercona/mysql-5.6
+cd -
 
 cd ${ORIG_PWD}
