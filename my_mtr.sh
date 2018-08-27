76e799042b0793a324b39a6433b06741bc0b2e7a
George O. Lorch III <george.lorch@percona.com>
2018-08-27 11:57:15 -0700

    Added ASAN preload.

--- my_mtr.sh
+++ my_mtr.sh
@@ -9,9 +9,10 @@ echo "Running on ${cpus} cpus"
 chmod +x ./mysql-test-run.pl
 LIBHOTBACKUP=$(find $(readlink -f ..) -type f -name 'libHotBackup.so')
 LIBEATMYDATA=$(find /usr/lib -name 'libeatmydata.so' | sort | head -n 1)
+LIBASAN=$(find /usr/lib -name 'libasan.so' | sort | head -n 1)
 if [ -z "$LIBEATMYDATA" ]; then
     echo "****** NO LIBEATMYDATA FOUND ******"
     exit 1
 fi
 
-LD_PRELOAD="${LIBEATMYDATA} ${LIBHOTBACKUP}" ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=120 --mysqld=--loose-tokudb-cache-size=512M --parallel=${cpus} $@
+LD_PRELOAD="${LIBASAN} ${LIBEATMYDATA} ${LIBHOTBACKUP}" ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=120 --mysqld=--loose-tokudb-cache-size=512M --parallel=${cpus} $@
--- tdb_mtr.sh
+++ tdb_mtr.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-if [ -n "${MAX_PARALLEL}" ]; then
-    cpus=${MAX_PARALLEL}
-else
-    cpus=`ncpus`
-fi
-echo "Running on ${cpus} cpus"
-
-chmod +x ./mysql-test-run.pl
-LIBHOTBACKUP=$(find $(readlink -f ..) -type f -name 'libHotBackup.so')
-LIBEATMYDATA=$(find /usr/lib -name 'libeatmydata.so' | sort | head -n 1)
-if [ -z "$LIBEATMYDATA" ]; then
-    echo "****** NO LIBEATMYDATA FOUND ******"
-    exit 1
-fi
-
-LD_PRELOAD="${LIBEATMYDATA} ${LIBHOTBACKUP}" ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=30 --mysqld=--loose-tokudb-cache-size=512M --parallel=${cpus} --big-test --suite=tokudb,tokudb.add_index,tokudb.alter_table,tokudb.bugs,tokudb.parts,tokudb.perfschema,tokudb.rpl,tokudb.sys_vars $@
+my_mtr.sh --suite=tokudb,tokudb.add_index,tokudb.alter_table,tokudb.bugs,tokudb.parts,tokudb.perfschema,tokudb.rpl,tokudb.sys_vars $@
