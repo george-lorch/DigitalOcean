edc1646b2d814c84af95ef14429bbf2cd8791970
George O. Lorch III <george.lorch@percona.com>
2018-09-12 09:30:31 -0700

    Fixing up my_mtr to better detect sanitize and preload libasan

--- my_mtr.sh
+++ my_mtr.sh
@@ -10,9 +10,20 @@ chmod +x ./mysql-test-run.pl
 LIBHOTBACKUP=$(find $(readlink -f ..) -type f -name 'libHotBackup.so')
 LIBEATMYDATA=$(find /usr/lib -name 'libeatmydata.so' | sort | head -n 1)
 LIBASAN=$(find /usr/lib -name 'libasan.so' | sort | head -n 1)
-if [ -z "$LIBEATMYDATA" ]; then
+if [ -z "${LIBEATMYDATA}" ]; then
     echo "****** NO LIBEATMYDATA FOUND ******"
     exit 1
 fi
 
-LD_PRELOAD="${LIBASAN} ${LIBEATMYDATA} ${LIBHOTBACKUP}" ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=120 --mysqld=--loose-tokudb-cache-size=512M --parallel=${cpus} $@
+PRELOAD="${LIBEATMYDATA}:${LIBHOTBACKUP}"
+
+echo $@ | grep "sanitize" > /dev/null
+if [ $? -eq 0 ]; then
+    if [ -z "${LIBASAN}" ]; then
+        echo "****** --sanitize specified but no libasan.so found ******"
+        exit 1
+    fi
+    PRELOAD="${LIBASAN}:${PRELOAD}"
+fi
+
+echo ./mtr --force --retry=0 --retry-failure=0 --max-test-fail=0 --testcase-timeout=120 --mysqld=--loose-tokudb-cache-size=512M --mysqld-env="LD_PRELOAD=${PRELOAD}" --parallel=${cpus} $@
