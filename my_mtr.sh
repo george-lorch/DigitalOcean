92d46daa46744171dad04dce4681ebf46b8d34b2
George O. Lorch III <george.lorch@percona.com>
2018-09-18 14:12:48 -0700

    Better handling of libhotbackup

--- my_mtr.sh
+++ my_mtr.sh
@@ -10,12 +10,24 @@ chmod +x ./mysql-test-run.pl
 LIBHOTBACKUP=$(find $(readlink -f ..) -type f -name 'libHotBackup.so')
 LIBEATMYDATA=$(find /usr/lib -name 'libeatmydata.so' | sort | head -n 1)
 LIBASAN=$(find /usr/lib -name 'libasan.so' | sort | head -n 1)
+
 if [ -z "${LIBEATMYDATA}" ]; then
     echo "****** NO LIBEATMYDATA FOUND ******"
     exit 1
 fi
+PRELOAD="${LIBEATMYDATA}"
 
-PRELOAD="${LIBEATMYDATA}:${LIBHOTBACKUP}"
+# check for any indication of hot backup, if none
+# skip loading the lib to try to keep logs a little cleaner
+echo $@ | grep "backup" > /dev/null
+if [ $? -eq 0 ]; then
+    if [ -z "${LIBHOTBACKUP}" ]; then
+        echo "****** backup detected but no libHotBackup.so found ******"
+        exit 1
+    fi
+    PRELOAD="${LIBHOTBACKUP}:${PRELOAD}"
+fi
+
 
 echo $@ | grep "sanitize" > /dev/null
 if [ $? -eq 0 ]; then
@@ -23,6 +35,7 @@ if [ $? -eq 0 ]; then
         echo "****** --sanitize specified but no libasan.so found ******"
         exit 1
     fi
+    # libasan must be first
     PRELOAD="${LIBASAN}:${PRELOAD}"
 fi
 
