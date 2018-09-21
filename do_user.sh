5d1327ce4eeb13adeed4c602b00576257c713141
George O. Lorch III <george.lorch@percona.com>
2018-09-21 07:49:59 -0700

    Fixed vim clang-format macros

--- do_user.sh
+++ do_user.sh
@@ -13,7 +13,7 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
 sudo apt-get update
-sudo apt-get install -y fail2ban vim screen git make cmake gcc g++ libncurses5-dev libreadline-dev bison libz-dev libgcrypt20 libgcrypt20-dev libssl-dev libboost-all-dev valgrind python-mysqldb mdm clang libasan5
+sudo apt-get install -y fail2ban vim screen git make cmake gcc g++ libncurses5-dev libreadline-dev bison libz-dev libgcrypt20 libgcrypt20-dev libssl-dev libboost-all-dev valgrind python-mysqldb mdm clang libasan5 clang-format
 
 ssh -o "StrictHostKeyChecking no" -T git@github.com
 git config --global user.email "george.lorch@percona.com"
@@ -36,7 +36,7 @@ echo -e "set expandtab\n" \
 "set sw=4\n" \
 "set softtabstop=4\n" \
 "set number\n" \
-"map <C-K> :py3f /usr/share/vim/addons/syntax/clang-format-60.py\n" \
-"imap <C-K> <c-o>:py3f /usr/share/vim/addons/syntax/clang-format-60.py\n" >> ~/.vimrc
+"map <C-K> :py3f /usr/share/vim/addons/syntax/clang-format.py\n" \
+"imap <C-K> <c-o>:py3f /usr/share/vim/addons/syntax/clang-format.py\n" >> ~/.vimrc
 
 cd ${ORIG_PWD}
