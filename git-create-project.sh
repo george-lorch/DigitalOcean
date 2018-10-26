6504fe95f0a31f82b1d3a8761ae786d44c42aed5
George O. Lorch III <george.lorch@percona.com>
2018-10-26 13:12:49 -0700

    Moved to individual repos for versions and updated cmake-*

--- git-create-project.sh
+++ git-create-project.sh
@@ -6,10 +6,11 @@ function wait_for_pid_to_disappear() {
     done
 }
 
-usage()
+function usage()
 {
     echo "Usage:"
-    echo "      $0 feature-name"
+    echo "      feature-name"
+    echo "      use-git-protocol"
 }
 
 PROJECT=$1
@@ -17,6 +18,11 @@ PROJECT=$1
 if [ -z "${PROJECT}" ]; then
     usage
     exit 1
+fi
+
+PROTOCOL="https://github.com/"
+if [ -n "${2}" ]; then
+    PROTOCOL="git@github.com:"
 fi
 
 if [ -d "./${PROJECT}" ]; then
@@ -46,38 +52,86 @@ ln -s ../boost_1_65_0
 ln -s ../boost_1_66_0
 ln -s ../boost_1_67_0
 
-git clone --recursive https://github.com/georgelorchpercona/myrocks &
-clone1=$!
+unset pids;
+pid_idx=1
 
-git clone --recursive https://github.com/percona/percona-server &
-clone2=$!
-mkdir percona-server-build-5.6
-mkdir percona-server-build-5.7
-mkdir percona-server-build-8.0
-mkdir percona-server-install-5.6
-mkdir percona-server-install-5.7
-mkdir percona-server-install-8.0
-mkdir perconaft-build
+git clone --recursive ${PROTOCOL}georgelorchpercona/myrocks &
+pids[${pid_idx}]=$!
+pid_idx=`expr ${pid_idx} + 1`
+
+git clone --recursive ${PROTOCOL}percona/percona-server -b 5.6 percona-server-5.6 &
+pids[${pid_idx}]=$!
+pid_idx=`expr ${pid_idx} + 1`
+
+git clone --recursive ${PROTOCOL}percona/percona-server -b 5.7 percona-server-5.7 &
+pids[${pid_idx}]=$!
+pid_idx=`expr ${pid_idx} + 1`
+
+git clone --recursive ${PROTOCOL}percona/percona-server -b 8.0 percona-server-8.0 &
+pids[${pid_idx}]=$!
+pid_idx=`expr ${pid_idx} + 1`
 
 git clone --recursive https://github.com/facebook/mysql-5.6 facebook-mysql &
-clone3=$!
+pids[${pid_idx}]=$!
+pid_idx=`expr ${pid_idx} + 1`
+
+mkdir percona-server-build-5.6
+mkdir percona-server-install-5.6
+
+mkdir percona-server-build-5.7
+mkdir percona-server-install-5.7
+
+mkdir percona-server-build-8.0
+mkdir percona-server-install-8.0
+
 mkdir facebook-mysql-build
 mkdir facebook-mysql-install
 
-wait_for_pid_to_disappear $clone1
-wait_for_pid_to_disappear $clone2
-wait_for_pid_to_disappear $clone3
+for ((i=1; i<${pid_idx}; i++)); do
+    wait_for_pid_to_disappear ${pids[${i}]}
+done
 
-cd ../${PROJECT}/percona-server
-git remote add downstream git@github.com:georgelorchpercona/percona-server
+unset pids
+unset pid_idx
+
+cd ../${PROJECT}/percona-server-5.6
+git remote add downstream ${PROTOCOL}georgelorchpercona/percona-server
+git remote add local-5.7 ../percona-server-5.7
+git remote add local-8.0 ../percona-server-8.0
+git remote add local-fb ../facebook-mysql
 cd -
 
-cd ../${PROJECT}/percona-server/storage/tokudb/PerconaFT
-git remote add downstream git@github.com:georgelorchpercona/PerconaFT
+cd ../${PROJECT}/percona-server-5.7
+git remote add downstream ${PROTOCOL}georgelorchpercona/percona-server
+git remote add local-5.7 ../percona-server-5.6
+git remote add local-8.0 ../percona-server-8.0
+git remote add local-fb ../facebook-mysql
+cd -
+
+cd ../${PROJECT}/percona-server-8.0
+git remote add downstream ${PROTOCOL}georgelorchpercona/percona-server
+git remote add local-5.6 ../percona-server-5.6
+git remote add local-5.7 ../percona-server-5.7
+git remote add local-fb ../facebook-mysql
+cd -
+
+cd ../${PROJECT}/percona-server-5.6/storage/tokudb/PerconaFT
+git remote add downstream ${PROTOCOL}georgelorchpercona/PerconaFT
+cd -
+
+cd ../${PROJECT}/percona-server-5.7/storage/tokudb/PerconaFT
+git remote add downstream ${PROTOCOL}georgelorchpercona/PerconaFT
+cd -
+
+cd ../${PROJECT}/percona-server-8.0/storage/tokudb/PerconaFT
+git remote add downstream ${PROTOCOL}georgelorchpercona/PerconaFT
 cd -
 
 cd ../${PROJECT}/facebook-mysql
-git remote add downstream git@github.com:georgelorchpercona/mysql-5.6
+git remote add downstream ${PROTOCOL}georgelorchpercona/mysql-5.6
+git remote add local-5.6 ../percona-server-5.6
+git remote add local-5.7 ../percona-server-5.7
+git remote add local-8.0 ../percona-server-8.0
 cd -
 
 cd ${ORIG_PWD}
