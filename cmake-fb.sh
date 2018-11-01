3c7f76a2f6839280eb8f85135a59b8c99d5c02ec
George O. Lorch III <george.lorch@percona.com>
2018-11-01 11:54:15 -0700

    Add MAX_PARALLEL to cmake-fb.sh

--- cmake-fb.sh
+++ cmake-fb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cpus=`ncpus`
+if [ -n "${MAX_PARALLEL}" ]; then
+    cpus=${MAX_PARALLEL}
+else
+    cpus=`ncpus`
+fi
 echo "Running on ${cpus} cpus"
 rm -rf *
 (cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSL=system -DWITH_ZLIB=bundled -DMYSQL_MAINTAINER_MODE=0 -DENABLED_LOCAL_INFILE=1 -DENABLE_DTRACE=0 -DCMAKE_CXX_FLAGS="-march=native" -DCMAKE_INSTALL_PREFIX=../facebook-mysql-install -DWITH_PERFSCHEMA_STORAGE_ENGINE=1 ../facebook-mysql $@ && make -j${cpus}) 2>&1 | tee build.log
