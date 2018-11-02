96bba536ae892fde41727c9fdbd8fe9db5b3c98d
George O. Lorch III <george.lorch@percona.com>
2018-11-02 09:50:45 -0700

    ed up git-create-gca to only work on one version at a time within the new multi-repo framework

--- git-create-gca.sh
+++ git-create-gca.sh
@@ -2,24 +2,49 @@
 
 set -e
 
-usage()
-{
+usage() {
     echo "Usage:"
-    echo "      $0 feature-name"
+    echo "      $0 feature-name version"
+}
+
+finished() {
+    git branch
+    exit 0
 }
 
 SERIES=( "5.6" "5.7" )
 FEATURE=$1
+VERSION=$2
+
+SERIES_TOP=$(( ${#SERIES[@]}-1 ))
+HIGHEST_VERSION=${SERIES[$SERIES_TOP]}
+
 
 if [ -z "${FEATURE}" ]; then
     usage
     exit 1
 fi
 
-array_top=$(( ${#SERIES[@]}-1 ))
-upper_version=${SERIES[$array_top]}
+if [ -z "${VERSION}" ]; then
+    usage
+    exit 1
+fi
 
-for i in `seq 0 ${array_top}`; do
+found=0
+for i in $(seq ${SERIES_TOP} -1 0); do
+    if [ "${SERIES[${i}]}" = "${VERSION}" ]; then
+        found=1
+        break
+    fi
+done
+
+if [ ${found} -eq 0 ]; then
+    echo "Unrecognized version \"${VERSION}\""
+    usage
+    exit 1
+fi
+
+for i in `seq 0 ${SERIES_TOP}`; do
     if [ -n "`git branch --list ps-${SERIES[$i]}-${FEATURE}`" ]; then
         echo "Feature branch ps-${SERIES[$i]}-${FEATURE} already exists. Aborting"
         exit 1
@@ -32,24 +57,30 @@ for series in ${SERIES}; do
     git checkout ${series}
     git pull
 done
-git checkout ${upper_version}
-git checkout -b ps-${upper_version}-${FEATURE}
-latest_commit=$(git log -n 1 --pretty=format:"%H" ${upper_version})
-echo "latest commit for ${upper_version} is ${latest_commit}"
-for i in $(seq ${array_top} -1 1) ; do
+git checkout ${HIGHEST_VERSION}
+if [ "${VERSION}" = "${HIGHEST_VERSION}" ]; then
+    git checkout -b ps-${HIGHEST_VERSION}-${FEATURE}
+    finished
+fi
+latest_commit=$(git log -n 1 --pretty=format:"%H" ${HIGHEST_VERSION})
+echo "latest commit for ${HIGHEST_VERSION} is ${latest_commit}"
+for i in $(seq ${SERIES_TOP} -1 1); do
     lower_version=${SERIES[$i-1]}
-    #commit=$(git merge-base ${upper_version} ${lower_version})
-    commit=$(git rev-list --topo-order --first-parent ${lower_version} ^${upper_version} | tail -1)
+    #commit=$(git merge-base ${HIGHEST_VERSION} ${lower_version})
+    commit=$(git rev-list --topo-order --first-parent ${lower_version} ^${HIGHEST_VERSION} | tail -1)
     if [ -z "$commit" ] ; then
         commit=${lower_version}
     else
         commit=$(git rev-parse ${commit}^)
     fi
     latest_commit=$(git log -n 1 --pretty=format:"%H" ${lower_version})
-    echo "GCA for ${upper_version} and ${lower_version} is ${commit}, latest commit for ${lower_version} is ${latest_commit}"
+    echo "GCA for ${HIGHEST_VERSION} and ${lower_version} is ${commit}, latest commit for ${lower_version} is ${latest_commit}"
     git checkout ${lower_version}
-    git checkout -b ps-${lower_version}-${FEATURE} ${commit}
-    upper_version=$commit
+    if [ "${VERSION}" = "${lower_version}" ]; then
+        git checkout -b ps-${lower_version}-${FEATURE} ${commit}
+        finished
+    fi
+    HIGHEST_VERSION=$commit
 done
 
-git branch
+finished
